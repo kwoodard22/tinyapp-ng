f810a27f69094d537974ee9b926c127b851b0cff
kwoodard22 <kwoodard22@gmail.com>
2017-05-29 22:04:24 -0700

    Fixed authToken error & toolbar login error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
 import { environment } from "../environments/environment";
 
 @Component({
@@ -7,20 +8,8 @@ import { environment } from "../environments/environment";
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
-  // constructor(private authToken: Angular2TokenService) {
-  //   this.authToken.init(environment.token_auth_config);
-
-  //   this.authToken.signIn({email: "kwoodard22@gmail.com", password: "password"}).subscribe(
-  //     res => {
-  //       console.log('auth response:', res);
-  //       console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-  //       console.log('auth response body:', res.json()); //log the response body to show the user 
-  //     },
-
-  //     err => {
-  //       console.error('auth error:', err);
-  //     }
-  //   )
-  // }
+  title = 'Yay for tiny!';
+  constructor(private authToken: Angular2TokenService) {
+    this.authToken.init(environment.token_auth_config);
+  }
 }
